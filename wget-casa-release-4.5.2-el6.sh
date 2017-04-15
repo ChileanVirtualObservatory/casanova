a87d5da06e5c9679e4f72c39ec2e1a9bccb046e6
Karl Heitmann <khf1988@gmail.com>
2017-04-15 14:04:06 -0300

    script added to download casa, gitignore updated, removed debuguing instruction on install_casanova script

---
+++ wget-casa-release-4.5.2-el6.sh
@@ -0,0 +1,1 @@
+wget https://casa.nrao.edu/download/distro/linux/release/el6/casa-release-4.5.2-el6.tar.gz
---
+++ wget-casa-release-4.7.2-el6.sh
@@ -0,0 +1,1 @@
+wget https://casa.nrao.edu/download/distro/linux/release/el6/casa-release-4.7.2-el6.tar.gz
